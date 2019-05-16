869d002a1b4027745b165cf5fe84127ffea22fb9
Leandro Fantinel <leandrofantinel@gmail.com>
2019-05-16 01:20:53 -0300

    update

--- filtrar.sh
+++ filtrar.sh
@@ -3,8 +3,12 @@
 origem=edson-flix.m3u
 destino=lista.m3u
 
-wget -O $origem http://bit.ly/EDSON-FLIX
+wget -O $origem http://bit.ly/MATRIXHD
+#http://bit.ly/EDSON-FLIX
 echo "#EXTM3U" > $destino
 grep -Pzo ".*group-title=\"(ESPORTES GLOBOSAT)\".*\nhttp.*\n" $origem >> $destino
 grep -Pzo ".*group-title=\"(PREMIERE FUTEBOL)\".*\nhttp.*\n" $origem >> $destino
 rm $origem
+git add .
+git commit -m "update"
+git push -u origem master
