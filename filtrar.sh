c778064c4467e61ff2c8fda82397fa6de2ea1fc1
Leandro Fantinel <leandrofantinel@gmail.com>
2019-05-16 02:06:02 -0300

    update

--- filtrar.sh
+++ filtrar.sh
@@ -1,12 +1,36 @@
 #!/bin/sh
 
 origem=edson-flix.m3u
-destino=lista.m3u
 
 wget --remote-encoding=UTF-8 -O $origem http://bit.ly/EDSON-FLIX
+
+destino=esportes.m3u
 echo "#EXTM3U" > $destino
-grep -Pzo ".*group-title=\"(ESPORTES.*)\".*\nhttp.*\n" $origem >> $destino
-grep -Pzo ".*group-title=\"(PREMIERE FUTEBOL)\".*\nhttp.*\n" $origem >> $destino
+grep -Pzo ".*group-title=\"ESPORTES.*\".*\nhttp.*\n" $origem >> $destino
+grep -Pzo ".*group-title=\"PREMIERE FUTEBOL\".*\nhttp.*\n" $origem >> $destino
+grep -Pzo ".*group-title=\"FUTEBOL AO VIVO\".*\nhttp.*\n" $origem >> $destino
+
+destino=filmes.m3u
+echo "#EXTM3U" > $destino
+grep -Pzo ".*group-title=\"FILMES/SERIES\".*\nhttp.*\n" $origem >> $destino
+grep -Pzo ".*group-title=\"REDE HBO\".*\nhttp.*\n" $origem >> $destino
+grep -Pzo ".*group-title=\"GLOBOSAT FILMES\".*\nhttp.*\n" $origem >> $destino
+
+destino=variedades.m3u
+echo "#EXTM3U" > $destino
+grep -Pzo ".*group-title=\"VARIEDADES\".*\nhttp.*\n" $origem >> $destino
+grep -Pzo ".*group-title=\"MUSICAS\".*\nhttp.*\n" $origem >> $destino
+
+destino=infantil.m3u
+echo "#EXTM3U" > $destino
+grep -Pzo ".*group-title=\"INFANTIL\".*\nhttp.*\n" $origem >> $destino
+
+destino=lancamentos.m3u
+echo "#EXTM3U" > $destinodestino=lancamentos.m3u
+echo "#EXTM3U" > $destino
+grep -Pzo ".*group-title=\"LANÇAMENTOS\".*\nhttp.*\n" $origem >> $destino
+grep -Pzo ".*group-title=\"LANÇAMENTOS\".*\nhttp.*\n" $origem >> $destino
+
 rm $origem
 git add .
 git commit -am "update"
