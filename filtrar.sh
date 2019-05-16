645745f2a84a49eebf098e4818f6de70a90d231c
Leandro Fantinel <leandrofantinel@gmail.com>
2019-05-16 01:25:00 -0300

    update

--- filtrar.sh
+++ filtrar.sh
@@ -6,7 +6,7 @@ destino=lista.m3u
 wget -O $origem http://bit.ly/MATRIXHD
 #http://bit.ly/EDSON-FLIX
 echo "#EXTM3U" > $destino
-grep -Pzo ".*group-title=\"(ESPORTES GLOBOSAT)\".*\nhttp.*\n" $origem >> $destino
+grep -Pzo ".*group-title=\"(ESPORTES)\".*\nhttp.*\n" $origem >> $destino
 grep -Pzo ".*group-title=\"(PREMIERE FUTEBOL)\".*\nhttp.*\n" $origem >> $destino
 rm $origem
 git add .
